08c4dcf046743d7c6940d498a515994798de9ed1
Marcin Gałąska <MG.asdfzxcv@gmail.com>
2024-02-24 21:39:45 +0100

    Add Polish and Russian localization (with help from xsSplater)

--- scripts/mods/show_remaining_dodges/hud/hud_element_dodging_settings.lua
+++ scripts/mods/show_remaining_dodges/hud/hud_element_dodging_settings.lua
@@ -1,4 +1,4 @@
--- Show Remaining Dodges mod by mroużon. Ver. 1.0.6
+-- Show Remaining Dodges mod by mroużon. Ver. 1.0.6a
 -- Thanks to Zombine, Redbeardt and others for their input into the community. Their work helped me a lot in the process of creating this mod.
 
 local hud_element_dodging_settings = {
--- scripts/mods/show_remaining_dodges/show_remaining_dodges_localization.lua
+++ scripts/mods/show_remaining_dodges/show_remaining_dodges_localization.lua
@@ -1,80 +1,132 @@
--- Show Remaining Dodges mod by mroużon. Ver. 1.0.6
+-- Show Remaining Dodges mod by mroużon. Ver. 1.0.6a
 -- Thanks to Zombine, Redbeardt and others for their input into the community. Their work helped me a lot in the process of creating this mod.
+
+-- Russian translation created with the help of xsSplater
 
 return {
 	mod_name = {
 		en = "Show Remaining Dodges",
+		ru = "Индикатор Оставшихся Уворотов",
+		pl = "Wskaźnik Pozostałych Uników"
 	},
 	mod_description = {
 		en = "Adds an in-game, stamina-like widget showing number of dodges remaining before dodging becomes ineffective.\n\nAuthor: mroużon",
+		ru = "Добавляет внутриигровой виджет, аналогичный виджету выносливости, отображающий количество оставшихся уворотов перед тем, как увороты станут неэффективными.\n\nАвтор: mroużon",
+		pl = "Dodaje podobny do wskaźnika kondycji widżet wyświetlający liczbę pozostałych uników, zanim unikanie stanie się nieefektywne.\n\nAutor: mroużon"
 	},
 	remaining_dodges_widget_settings_behaviour = {
-		en = "Widget Behaviour"
+		en = "Widget Behaviour",
+		ru = "Поведение Индикатора",
+		pl = "Zachowanie Wskaźnika"
 	},
 	remaining_dodges_widget_fade_inout_speed = {
-		en = "Fade In/Out Speed"
+		en = "Fade In/Out Speed",
+		ru = "Скорость Появления/Исчезновения",
+		pl = "Prędkość Pojawiania Się/Zanikania"
 	},
 	remaining_dodges_widget_fade_inout_speed_desc = {
-		en = "How quickly the widget will fade in and out."
+		en = "How quickly the widget will fade in and out.",
+		ru = "Как быстро будет появляться и исчезать индикатор.",
+		pl = "Jak szybko wskaźnik będzie się pojawiał i znikał."
 	},
 	remaining_dodges_widget_bar_settings_appearance = {
-		en = "Widget Bars Style"
+		en = "Widget Bars Style",
+		ru = "Стиль Баров Индикатора",
+		pl = "Styl Pasków Wskaźnika"
 	},
 	remaining_dodges_widget_bar_R = {
-		en = "Red"
+		en = "Red",
+		ru = "Красный",
+		pl = "Czerwony"
 	},
 	remaining_dodges_widget_bar_R_desc = {
-		en = "Intensity of the color red."
+		en = "Intensity of the color red.",
+		ru = "Интенсивность Красного цвета",
+		pl = "Intensywność koloru czerwonego."
 	},
 	remaining_dodges_widget_bar_G = {
-		en = "Green"
+		en = "Green",
+		ru = "Зелёный",
+		pl = "Zielony"
 	},
 	remaining_dodges_widget_bar_G_desc = {
-		en = "Intensity of the color green."
+		en = "Intensity of the color green.",
+		ru = "Интенсивность Зелёного цвета",
+		pl = "Intensywność koloru zielonego."
 	},
 	remaining_dodges_widget_bar_B = {
-		en = "Blue"
+		en = "Blue",
+		ru = "Синий",
+		pl = "Niebieski"
 	},
 	remaining_dodges_widget_bar_B_desc = {
-		en = "Intensity of the color blue."
+		en = "Intensity of the color blue.",
+		ru = "Интенсивность Синего цвета",
+		pl = "Intensywność koloru niebieskiego."
 	},
 	remaining_dodges_widget_text_settings_appearance = {
-		en = "Widget Text Style"
+		en = "Widget Text Style",
+		ru = "Стиль Текста Индикатора",
+		pl = "Styl Tekstu Wskaźnika"
 	},
 	remaining_dodges_widget_text_R = {
-		en = "Red"
+		en = "Red",
+		ru = "Красный",
+		pl = "Czerwony"
 	},
 	remaining_dodges_widget_text_R_desc = {
-		en = "Intensity of the color red."
+		en = "Intensity of the color red.",
+		ru = "Интенсивность Красного цвета",
+		pl = "Intensywność koloru czerwonego."
 	},
 	remaining_dodges_widget_text_G = {
-		en = "Green"
+		en = "Green",
+		ru = "Зелёный",
+		pl = "Zielony"
 	},
 	remaining_dodges_widget_text_G_desc = {
-		en = "Intensity of the color green."
+		en = "Intensity of the color green.",
+		ru = "Интенсивность Зелёного цвета",
+		pl = "Intensywność koloru zielonego."
 	},
 	remaining_dodges_widget_text_B = {
-		en = "Blue"
+		en = "Blue",
+		ru = "Синий",
+		pl = "Niebieski"
 	},
 	remaining_dodges_widget_text_B_desc = {
-		en = "Intensity of the color blue."
+		en = "Intensity of the color blue.",
+		ru = "Интенсивность Синего цвета",
+		pl = "Intensywność koloru niebieskiego."
 	},
 	remaining_dodges_widget_settings_position = {
-		en = "Widget Position"
+		en = "Widget Position",
+		ru = "Положение Индикатора",
+		pl = "Pozycja Wskaźnika"
 	},
 	remaining_dodges_widget_vertical_offset = {
-		en = "Vertical Offset"
+		en = "Vertical Offset",
+		ru = "Вертикальное Смещение",
+		pl = "Przesunięcie Pionowe"
 	},
 	remaining_dodges_widget_vertical_offset_desc = {
-		en = "Offset applied to the widget in the Y axis."
+		en = "Offset applied to the widget in the Y axis.",
+		ru = "Смещение, применённое к индикатору по оси Y.",
+		pl = "Przesunięcie wskaźnika w osi Y."
 	},
 	remaining_dodges_widget_horizontal_offset = {
-		en = "Horizontal Offset"
+		en = "Horizontal Offset",
+		ru = "Горизонтальное Смещение",
+		pl = "Przesunięcie Poziome"
 	},
 	remaining_dodges_widget_horizontal_offset_desc = {
-		en = "Offset applied to the widget in the X axis."
+		en = "Offset applied to the widget in the X axis.",
+		ru = "Смещение, применённое к индикатору по оси X.",
+		pl = "Przesunięcie wskaźnika w osi X."
 	},
 	hud_display_name_dodges = {
-		en = "Dodges"
+		en = "Dodges",
+		ru = "Увороты",
+		pl = "Uniki"
 	}
 }
